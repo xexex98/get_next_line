a80571eee424b91e9706f23a789559128737d7b0
mbarra <mbarra@student.21-school.ru>
2021-11-04 13:38:42 +0300

    5_1

---
+++ gnl_beta/get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbarra <mbarra@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/27 13:18:39 by mbarra            #+#    #+#             */
+/*   Updated: 2021/10/31 21:48:28 by mbarra           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef __GET_NEXT_LINE_H__
+# define __GET_NEXT_LINE_H__
+// # define BUFFER_SIZE 2
+// #ifndef BUFFER_SIZE
+// # define BUFFER_SIZE 2
+// #endif
+char	*get_next_line(int fd);
+
+#endif
